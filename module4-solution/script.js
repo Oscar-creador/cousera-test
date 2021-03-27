d93168e4c25666433193b53235fc802dcc678a24
Clemente <1930672@upv.edu.mx>
2021-03-27 13:04:08 -0600

    delete

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -41,7 +41,7 @@ WARNING!!! WARNING!!!
 (function(){
 
   
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"],
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
 // STEP 10:
